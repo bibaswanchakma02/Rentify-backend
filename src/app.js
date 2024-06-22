0761c7168e013c34897d3babdfd040fff207b020
Bibaswan Chakma <bibaswanch1769@gmail.com>
2024-06-23 00:16:01 +0530

    User authentication - api endpoints /register, /login and /logout configured. JWT authentication used

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser';
+import authRoute from "./routes/auth.route.js"
+import postRoute from './routes/post.route.js'
+
 
 const app = express();
 dotenv.config()
 
-app.use("/api/test",(req,res)=>{
-    res.send("successful")
-})
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/post", postRoute) 
+app.use("/api/auth", authRoute) 
+
 
 app.listen( process.env.PORT, ()=>{
     console.log(`Server is running on port: ${process.env.PORT}`);
 })
+
---
+++ src/routes/post.route.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const router = express.Router()
+
+// router.get("/test", (req,res) =>{
+//     console.log("post route works");
+// })
+// router.post("/test", (req,res) =>{
+//     console.log("post route works");
+// })
+// router.put("/test", (req,res) =>{
+//     console.log("post route works");
+// })
+// router.delete("/test", (req,res) =>{
+//     console.log("post route works");
+// })
+
+export default router
